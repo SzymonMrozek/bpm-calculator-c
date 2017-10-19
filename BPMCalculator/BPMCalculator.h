dd8b6cef5c0fd6ec4d9ed7e21b5bd96bdac38b69
SzymonMrozek <szymon.mrozek.sm@gmail.com>
2017-10-19 12:25:38 +0200

    Initial Commit

---
+++ BPMCalculator/BPMCalculator.h
@@ -0,0 +1,13 @@
+//
+//  BPMCalculator.h
+//  BPMCalculator
+//
+//  Created by Szymon Mrozek on 19.10.2017.
+//  Copyright © 2017 Szymon Mrozek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BPMCalculator : NSObject
+
+@end
---
+++ BPMCalculator/BPMCalculator.m
@@ -0,0 +1,13 @@
+//
+//  BPMCalculator.m
+//  BPMCalculator
+//
+//  Created by Szymon Mrozek on 19.10.2017.
+//  Copyright © 2017 Szymon Mrozek. All rights reserved.
+//
+
+#import "BPMCalculator.h"
+
+@implementation BPMCalculator
+
+@end
